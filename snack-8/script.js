3a5ddb24711bb94bd0904f7b0335fd98a619cc3f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-21 16:07:45 +0100

    8th task done

--- snack-8/script.js
+++ snack-8/script.js
@@ -22,25 +22,6 @@ const students = [
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
 
-// ** Prima applico map per eliminare i dati non necessari (id & age)
-
-const studentsFormatted = students.map((student) => {
-  const { name, classe } = student;
-  newObjs = {
-    name,
-    classe,
-  };
-
-  return newObjs;
+const findClass = students.find(({ classe, name }) => {
+  if (name === "Marco Lanci") console.log(classe);
 });
-
-console.table(studentsFormatted);
-
-// ** Ora uso find per recuperare la classe
-
-const findClass = studentsFormatted.find((student) => {
-  const { classe, name } = student;
-  if (name === "Marco Lanci") return classe;
-});
-
-console.log(findClass);
