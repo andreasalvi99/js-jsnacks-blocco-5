0951b3e94e94ec4f93b23becdc82c441dbcf270a
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-21 15:57:25 +0100

    help

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,46 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    classe: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    classe: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    classe: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// ** Prima applico map per eliminare i dati non necessari (id & age)
+
+const studentsFormatted = students.map((student) => {
+  const { name, classe } = student;
+  newObjs = {
+    name,
+    classe,
+  };
+
+  return newObjs;
+});
+
+console.table(studentsFormatted);
+
+// ** Ora uso find per recuperare la classe
+
+const findClass = studentsFormatted.find((student) => {
+  const { classe, name } = student;
+  if (name === "Marco Lanci") return classe;
+});
+
+console.log(findClass);
